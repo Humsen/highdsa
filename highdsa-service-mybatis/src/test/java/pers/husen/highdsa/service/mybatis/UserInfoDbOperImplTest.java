52c478fdebfb57aef90d5ad954ca5218ab6889dd
何明胜 <husen@hemingsheng.cn>
2018-03-09 14:06:59 +0800

    1.整合druid数据库连接池,配置log4j2输出;

--- highdsa-service-mybatis/src/test/java/pers/husen/highdsa/service/mybatis/UserInfoDbOperImplTest.java
+++ highdsa-service-mybatis/src/test/java/pers/husen/highdsa/service/mybatis/UserInfoDbOperImplTest.java
@@ -1,9 +1,13 @@
 package pers.husen.highdsa.service.mybatis;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import pers.husen.highdsa.common.entity.po.UserInfo;
 
@@ -17,7 +21,18 @@ import pers.husen.highdsa.common.entity.po.UserInfo;
  * @Version 1.0.0
  */
 public class UserInfoDbOperImplTest {
-	UserInfoDbOperImpl userInfoDbOperImpl = new UserInfoDbOperImpl();
+	private UserInfoDbOperImpl userInfoDbOperImpl;
+	private ClassPathXmlApplicationContext context;
+	
+	@Before
+	public void before() {
+		String[] configLocation = new String[] { "spring/mybatis-provider.xml", "spring/redis-consumer.xml","spring/spring-context.xml" };
+
+		context = new ClassPathXmlApplicationContext(configLocation);
+		context.start();
+		userInfoDbOperImpl = (UserInfoDbOperImpl) context.getBean("userInfoDbOperImpl");
+		System.out.println("=============== dubbo已经启动... ==================");
+	}
 	
 	@Test
 	public void selectById() {
@@ -55,5 +70,18 @@ public class UserInfoDbOperImplTest {
 	@Test
 	public void deleteUserInfo() {
 		userInfoDbOperImpl.deleteUserInfo(3);
+	}
+	
+	@After
+	public void after() {
+		try {
+			// 为保证服务一直开着，利用输入流的阻塞来模拟
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("=============== 消费者关闭... ==================");
+		context.close();
 	}
 }
